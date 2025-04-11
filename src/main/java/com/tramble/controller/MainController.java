d0573c8802b15259ad34072b827571e4f3ae7d1e
tramble <jmk3796@gmail.com>
2025-04-11 16:20:00 +0900

    commit 4

--- src/main/java/com/tramble/controller/MainController.java
+++ src/main/java/com/tramble/controller/MainController.java
@@ -11,7 +11,7 @@ public class MainController {
 
 	@GetMapping
 	public String intro(Model model) {
-		model.addAttribute("greeting", "안녕하세요");
+		model.addAttribute("greeting", "젠킨스를 이용한 CI/CD");
 		return "intro";
 		
 	}
