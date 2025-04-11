11a3804841d4a17dd5352917be898669c88c5377
tramble <jmk3796@gmail.com>
2025-04-11 09:29:37 +0900

    first commit

---
+++ src/test/java/com/tramble/JenkinscicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tramble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinscicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
